ce176413023016b845c6ba9e4b1061fd95457d25
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-13 11:02:18 +0000

    use develop branch

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "flux" {
   git = {
     url = "https://github.com/${var.github_repository}.git"
     http = {
+      branch = "develop"
       username = "git"
       password = var.github_token
     }
